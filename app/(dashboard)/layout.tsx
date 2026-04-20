98b6dbf00d5b599f8456131142f5367d299a9df1
enverhoke <ender.hokee@gmail.com>
2026-04-20 15:23:07 +0300

    Delete app directory

--- app/(dashboard)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { RequireAuth } from '@/components/RequireAuth';
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <RequireAuth>{children}</RequireAuth>;
-}
